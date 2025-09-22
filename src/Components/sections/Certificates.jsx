cd28807a727475cc803839da2d657015dfdbb276
pankaj143p <pankaj114477pankaj@gmail.com>
2025-09-22 21:58:08 +0530

    components have arranged

--- src/Components/sections/Certificates.jsx
+++ src/Components/sections/Certificates.jsx
@@ -1,1 +1,25 @@
-// ...existing code from /src/Components/Certificates.jsx...
+import React from 'react';
+import Certificate1 from '../../assets/certificates/c1.jpg';
+import Certificate2 from '../../assets/certificates/c2.jpg';
+import Certificate3 from '../../assets/certificates/c3.jpeg';
+import Certificate4 from '../../assets/certificates/c4.png';
+import Certificate8 from '../../assets/certificates/c8.jpg';
+import Certificate9 from '../../assets/certificates/c9.jpg';
+
+const Certificates = () => {
+    return (
+        <div>
+            <h1>My Certificates</h1>
+            <div>
+                <img src={Certificate1} alt="Certificate 1" />
+                <img src={Certificate2} alt="Certificate 2" />
+                <img src={Certificate3} alt="Certificate 3" />
+                <img src={Certificate4} alt="Certificate 4" />
+                <img src={Certificate8} alt="Certificate 8" />
+                <img src={Certificate9} alt="Certificate 9" />
+            </div>
+        </div>
+    );
+};
+
+export default Certificates;
--- src/Components/sections/Projects.jsx
+++ src/Components/sections/Projects.jsx
@@ -1,1 +1,23 @@
-// ...existing code from /src/Components/Projects.jsx...
+// ...existing code from /src/Components/Projects.jsx
+import project1 from '../../assets/Projects/marketmart.png';
+import project2 from '../../assets/Projects/quiz.avif';
+import project3 from '../../assets/Projects/education.png';
+import project4 from '../../assets/Projects/weather.png';
+import project5 from '../../assets/Projects/chat.png';
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <div>
+        <img src={project1} alt="MarketMart Project" />
+        <img src={project2} alt="Quiz App Project" />
+        <img src={project3} alt="Education Platform Project" />
+        <img src={project4} alt="Weather App Project" />
+        <img src={project5} alt="Chat Application Project" />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
