11a47d82237d6a897741346906b9c1480ce1be37
pankaj143p <pankaj114477pankaj@gmail.com>
2026-01-06 22:05:55 +0530

    changes in hero section and created error boundary component

---
+++ src/Components/effects/index.js
@@ -0,0 +1,5 @@
+export { default as CustomCursor } from './CustomCursor';
+export { default as ParticleBackground } from './ParticleBackground';
+export { default as ScrollProgress } from './ScrollProgress';
+export { default as ScrollToTop } from './ScrollToTop';
+export { default as LoadingScreen } from './LoadingScreen';
---
+++ src/Components/sections/index.js
@@ -0,0 +1,3 @@
+export { default as Experience } from './Experience';
+export { default as AnimatedStats } from './AnimatedStats';
+export { default as Testimonials } from './Testimonials';
